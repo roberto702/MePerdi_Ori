3c9298c4f896e2c4fd125c3eff014c31baf124ff
angela7319 <robmath702@hotmail.com>
2016-08-23 00:41:20 -0300

    Initial commit

---
+++ app/src/main/java/cl/micasa/meperdi/MisPublicacionesActivity.java
@@ -0,0 +1,37 @@
+package cl.micasa.meperdi;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MisPublicacionesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout_mis_publicaciones_main);
+
+        Button btn_publicar = (Button) findViewById(R.id.button_publicar);
+        btn_publicar.setOnClickListener(new View.OnClickListener() {
+            @Override public void onClick(View v) { Intent intent = new Intent (v.getContext(), LoginActivity.class); startActivityForResult(intent, 0);
+            } });
+    }
+
+    public void onClickImg1(View view) {
+        Toast toast1 = Toast.makeText(getApplicationContext(), "Compartir imagen 1", Toast.LENGTH_SHORT);
+        toast1.setGravity(Gravity.CENTER, 0, 0);
+        toast1.show();
+
+    }
+
+    public void onClickImg2(View view) {
+        Toast toast1 = Toast.makeText(getApplicationContext(), "Compartir imagen 2", Toast.LENGTH_SHORT);
+        toast1.setGravity(Gravity.CENTER, 0, 0);
+        toast1.show();
+
+    }
+    }
---
+++ app/src/main/java/cl/micasa/meperdi/PedirDatosActivity.java
@@ -0,0 +1,13 @@
+package cl.micasa.meperdi;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PedirDatosActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout_pedir_datos);
+    }
+}
