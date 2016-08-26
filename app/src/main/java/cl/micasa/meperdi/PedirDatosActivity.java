c9a5ffb78ba5fd8ca0dc565d191ea13c94727b75
roberto702 <robmath702@hotmail.com>
2016-08-25 22:07:27 -0300

    Se ingresa codigo para ingreso de datos en PedirDatosActivity.java, ademas se actualiza el layaut_pedir_datos.xml
    Debo corregir los errores que me envia al momento de compilar

--- app/src/main/java/cl/micasa/meperdi/PedirDatosActivity.java
+++ app/src/main/java/cl/micasa/meperdi/PedirDatosActivity.java
@@ -1,7 +1,9 @@
 package cl.micasa.meperdi;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.TextInputLayout;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.text.Editable;
@@ -32,7 +34,7 @@ public class PedirDatosActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout._pedir_datos);
+        setContentView(R.layout_pedir_datos);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         ActionBar actionBar = getSupportActionBar();
