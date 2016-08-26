453483dcc1c8a3c4d0a8b8af3bd05513b4c61e17
roberto702 <robmath702@hotmail.com>
2016-08-25 22:03:11 -0300

    Se ingresa codigo para ingreso de datos en PedirDatosActivity.java, ademas se actualiza el layaut_pedir_datos.xml
    Debo corregir los errores que me envia al momento de compilar

--- app/src/main/java/cl/micasa/meperdi/PedirDatosActivity.java
+++ app/src/main/java/cl/micasa/meperdi/PedirDatosActivity.java
@@ -1,13 +1,282 @@
 package cl.micasa.meperdi;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.TextInputLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+import java.util.regex.Pattern;
 
 public class PedirDatosActivity extends AppCompatActivity {
+
+    private TextInputLayout tilNombre;
+    private TextInputLayout tilTelefono;
+    private TextInputLayout tilCorreo;
+    private TextInputLayout tilNombre_mascota;
+    private TextInputLayout tilTipo_animal;
+    private TextInputLayout tilCaracteristica;
+
+    private EditText campoNombre;
+    private EditText campoTelefono;
+    private EditText campoCorreo;
+    private EditText campoNombre_mascota;
+    private EditText campoTipo_animal;
+    private EditText campoCaracterista;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.layout_pedir_datos);
+        setContentView(R.layout._pedir_datos);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setLogo(R.mipmap.ic_launcher);
+
+        // Referencias TILs
+        tilNombre = (TextInputLayout) findViewById(R.id.til_nombre);
+        tilTelefono = (TextInputLayout) findViewById(R.id.til_telefono);
+        tilCorreo = (TextInputLayout) findViewById(R.id.til_correo);
+        tilNombre_mascota = (TextInputLayout) findViewById(R.id.til_nombre_mascota);
+        tilTipo_animal = (TextInputLayout) findViewById(R.id.til_tipo_animal);
+        tilCaracteristica = (TextInputLayout) findViewById(R.id.til_caracteristica);
+
+        // Referencias ETs
+        campoNombre = (EditText) findViewById(R.id.campo_nombre);
+        campoTelefono = (EditText) findViewById(R.id.campo_telefono);
+        campoCorreo = (EditText) findViewById(R.id.campo_correo);
+        campoNombre_mascota = (EditText) findViewById(R.campo_nombre_mascota);
+        campoTipo_animal = (EditText) findViewById(R.campo_tipo_animal);
+        campoCaracterista = (EditText) findViewById(R.campo_caracteristica);
+
+        campoNombre.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                tilNombre.setError(null);
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
+        campoTelefono.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                tilTelefono.setError(null);
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
+        campoCorreo.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                esCorreoValido(String.valueOf(s));
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
+        campoNombre_mascota.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                tilNombre_mascota.setError(null);
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
+        campoTipo_animal.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                tilTipo_animal.setError(null);
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
+        campoCaracterista.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                tilCaracteristica.setError(null);
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
+        // Referencia Botón
+        Button botonAceptar = (Button) findViewById(R.id.boton_aceptar);
+        botonAceptar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                validarDatos();
+            }
+        });
+
+        Button botonCancelar = (Button) findViewById(R.id.boton_cancelar);
+        botonCancelar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //accion al presionar boton cancelar
+                finish();
+                Intent intent = new Intent(Intent.ACTION_MAIN);
+                intent.addCategory(Intent.CATEGORY_HOME);
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(intent);
+            }
+
+        });
+
+
     }
+
+
+    private boolean esNombreValido(String nombre) {
+        Pattern patron = Pattern.compile("^[a-zA-Z ]+$");
+        if (!patron.matcher(nombre).matches() || nombre.length() > 30) {
+            tilNombre.setError("Nombre inválido");
+            return false;
+        } else {
+            tilNombre.setError(null);
+        }
+
+        return true;
+    }
+
+    private boolean esTelefonoValido(String telefono) {
+        if (!Patterns.PHONE.matcher(telefono).matches()) {
+            tilTelefono.setError("Teléfono inválido");
+            return false;
+        } else {
+            tilTelefono.setError(null);
+        }
+
+        return true;
+    }
+
+    private boolean esCorreoValido(String correo) {
+        if (!Patterns.EMAIL_ADDRESS.matcher(correo).matches()) {
+            tilCorreo.setError("Correo electrónico inválido");
+            return false;
+        } else {
+            tilCorreo.setError(null);
+        }
+
+        return true;
+    }
+
+    private boolean esNombremascotaValido(String nombre_mascota) {
+        Pattern patron = Pattern.compile("^[a-zA-Z ]+$");
+        if (!patron.matcher(nombre_mascota).matches() || nombre_mascota.length() > 30) {
+            tilNombre_mascota.setError("Nombre inválido");
+            return false;
+        } else {
+            tilNombre_mascota.setError(null);
+        }
+
+        return true;
+    }
+
+    private boolean esTipoMascotaValido(String tipo_animal) {
+        Pattern patron = Pattern.compile("^[a-zA-Z ]+$");
+        if (!patron.matcher(tipo_animal).matches() || tipo_animal.length() > 30) {
+            tilTipo_animal.setError("Nombre inválido");
+            return false;
+        } else {
+            tilTipo_animal.setError(null);
+        }
+
+        return true;
+    }
+
+    private boolean esCaracteristicaValido(String caracteristica) {
+        Pattern patron = Pattern.compile("^[a-zA-Z ]+$");
+        if (!patron.matcher(caracteristica).matches() || caracteristica.length() > 30) {
+            tilCaracteristica.setError("Nombre inválido");
+            return false;
+        } else {
+            tilCaracteristica.setError(null);
+        }
+
+        return true;
+    }
+
+    private void validarDatos() {
+        String nombre = tilNombre.getEditText().getText().toString();
+        String telefono = tilTelefono.getEditText().getText().toString();
+        String correo = tilCorreo.getEditText().getText().toString();
+        String nombre_mascota = tilNombre_mascota.getEditText().getText().toString();
+        String tipo_animal = tilTipo_animal.getEditText().getText().toString();
+        String caracteristica = tilCaracteristica.getEditText().getText().toString();
+
+        boolean a = esNombreValido(nombre);
+        boolean b = esTelefonoValido(telefono);
+        boolean c = esCorreoValido(correo);
+        boolean d = esNombremascotaValido(nombre_mascota);
+        boolean e = esTipoMascotaValido(tipo_animal);
+        boolean f = esCaracteristicaValido(caracteristica);
+
+        if (a && b && c && d && e && f) {
+            // OK, se pasa a la siguiente acción
+            Toast.makeText(this, "Se guarda el registro", Toast.LENGTH_LONG).show();
+        }
+
+    }
+
 }
+
+
+
+
